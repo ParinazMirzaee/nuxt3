90c210b87a5771644291f9f816969c4bb121b69d
Parinaz <mpmirzaee2325@yahoo.com>
2023-11-19 10:17:40 +0330

    add several components

---
+++ component/Car/Details/Detail.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mt-10">
+    <img
+        src="https://carwow-uk-wp-3.imgix.net/Volvo-XC40-white-scaled.jpg"
+        class="w-50"
+        alt=""
+    />
+    <h1 class="mt-3">Volvo 3XT</h1>
+    <div
+        class="d-flex text-lg mt-3 border pb-5 justify-content-between"
+    >
+      <div class="d-flex">
+        <p class="mx-2">5 seats</p>
+        <p class="mx-2">|</p>
+        <p class="mx-2">67,444 miles</p>
+      </div>
+      <div>
+        <p class="font-bold ">$25,555</p>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/car/[name]-[id].vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div
+        class="mx-auto mt-4 px-4 pb-3"
+    >
+      <Detail/>
+      <Attributes/>
+      <Description/>
+      <Contact/>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import Detail from "~/component/Car/Details/Detail.vue";
+import Attributes from "~/component/Car/Details/Attributes.vue";
+import Description from "~/component/Car/Details/Description.vue";
+import Contact from "~/component/Car/Details/Contact.vue";
+</script>

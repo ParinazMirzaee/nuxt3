12daa9b0e205f2b9588cb66d286865b7b9efd7db
Parinaz <mpmirzaee2325@yahoo.com>
2023-11-19 09:13:27 +0330

    Card and Cards components

---
+++ component/Car/Cards.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="w-full">
+    <Card/>
+  </div>
+</template>
+<script setup lang="ts">
+import Card from "~/component/Car/Card.vue";
+</script>
--- pages/city/[city]/car/[[make]].vue
+++ pages/city/[city]/car/[[make]].vue
@@ -1,7 +1,19 @@
 <template>
   <div>
-    <h1>
-      city and make
-    </h1>
+    <NavBar/>
+    <div
+        class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
+    >
+      <div class="mt-5 d-flex">
+        <SideBar/>
+        <Cards/>
+      </div>
+    </div>
+
   </div>
 </template>
+<script setup lang="ts">
+import NavBar from "~/component/NavBar.vue";
+import SideBar from "~/component/Car/SideBar.vue";
+import Card from "~/component/Car/Card.vue";
+</script>

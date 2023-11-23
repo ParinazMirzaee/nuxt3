7ee937cc9ffe305976694c624b1bb45c7080c3a2
Parinaz <mpmirzaee2325@yahoo.com>
2023-11-23 11:41:18 +0330

    make a car your favorite

--- component/Car/Card.vue
+++ component/Car/Card.vue
@@ -1,21 +1,29 @@
 <script setup>
+import heartFilled from "~/assets/002 heartFilled.png"
+import heartOutline from "~/assets/002 heartOutline.png"
+import {useState} from "#app";
 const props = defineProps({
   car: {
     type: Object
   }
 })
+const favored = useState(`favored-${props.car.id}`, ()=>{
+  return false
+})
 </script>
 
 <template>
   <div
-      class="shadow border overflow-hidden m-5 cursor-pointer w-50"
-      @click="navigateTo(`/car/${car.name}-${car.id}`)"
+      class="position-relative shadow border overflow-hidden m-5 cursor-pointer h-25"
   >
+    <img class="position-absolute w-5 cursor" :src="favored ? heartFilled : heartOutline" alt="" @click="favored = !favored">
+
     <div class="d-flex">
       <NuxtImg
           :src="car.url"
           alt=""
-          class="w-25 aspect-ratio"
+          class="w-25 aspect-ratio cursor"
+          @click="navigateTo(`/car/${car.name}-${car.id}`)"
       />
       <div class="p-4 d-flex">
         <div>
@@ -31,5 +39,10 @@ const props = defineProps({
 </template>
 
 <style scoped>
+.w-5{
+  width: 25px;
+  right: 20px;
+  top: 20px;
+}
 
 </style>

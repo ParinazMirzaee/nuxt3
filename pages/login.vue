464868fd498ddac33fa8cb35a98c961f07a1aa2a
Parinaz <mpmirzaee2325@yahoo.com>
2023-11-26 22:09:58 +0330

    add authentication with supabase

---
+++ pages/login.vue
@@ -0,0 +1,57 @@
+<script setup>
+const supabase = useSupabaseClient()
+const user = useSupabaseUser()
+
+async function signUpNewUser() {
+  const { data, error } = supabase.auth.signUp({
+    email: 'example@email.com',
+    password: 'example-password',
+    options: {
+      emailRedirectTo: 'https//example.com/welcome'
+    }
+  })
+}
+async function signInWithEmail() {
+  const { data, error } = supabase.auth.signInWithPassword({
+    email: 'example@email.com',
+    password: 'example-password'
+  })
+}
+
+async function signOut() {
+  const { error } = supabase.auth.signOut()
+}
+
+
+
+/*const login = async () => {
+  const {error} = supabase.auth.signInWithOAuth({
+    provider: "github"
+  });
+  if (error) {
+    console.log(error);
+  }
+}*/
+
+</script>
+
+<template>
+  <NuxtLayout name="custom">
+    <div>
+      <h1 class="font-bold">Login</h1>
+      <button @click="signUpNewUser" class="bg-secondary border-0 rounded text-white">
+        signup with email
+      </button>
+      <button @click="signInWithEmail" class="bg-primary border-0 rounded text-white">
+        login
+      </button>
+      <button @click="signOut" class="bg-danger border-0 rounded text-white">
+        signOut
+      </button>
+    </div>
+  </NuxtLayout>
+</template>
+
+<style scoped>
+
+</style>

776a3c1b7dd77b0ef5a9ebce8e8d85b6caaf7e3b
Parinaz <mpmirzaee2325@yahoo.com>
2023-11-27 10:23:15 +0330

    extract the path parameters

--- server/api/cars/[city].js
+++ server/api/cars/[city].js
@@ -1,3 +1,11 @@
-export default defineEventHandler((event)=>{
-    return "Hello World!!!!";
+import cars from "~/data/cars.json";
+
+export default defineEventHandler((event) => {
+    const {city} = event.context.params;
+
+    let filteredCars = cars;
+    filteredCars = filteredCars.filter((cars) => {
+        return cars.city.toLowerCase() === city.toLowerCase();
+    });
+    return filteredCars;
 })

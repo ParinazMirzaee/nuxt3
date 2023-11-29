e230e06fb5b7657fddf1795e22a54b31435e3aea
Parinaz <mpmirzaee2325@yahoo.com>
2023-11-29 10:17:36 +0330

    fetch data from client and refresh the page and recall api after apply price range

--- pages/city/[city]/car/[[make]].vue
+++ pages/city/[city]/car/[[make]].vue
@@ -2,15 +2,20 @@
 import Cards from "~/component/Car/Cards.vue";
 
 const route = useRoute();
-const cars = await useFetchCars(route.params.city, {
+const {data: cars, refresh} = await useFetchCars(route.params.city, {
   minPrice: route.query.minPrice,
   maxPrice: route.query.maxPrice,
   make: route.params.make,
 });
+
+watch(() => route.query, () => {
+  window.location.reload(true);
+})
 </script>
 
 <template>
   <div>
-    <Cards :cars="cars"/>
+    <Cards v-if="cars.length" :cars="cars"/>
+    <h3 class="ms-3 mt-5 text-danger" v-else>No cars found with filters</h3>
   </div>
 </template>
